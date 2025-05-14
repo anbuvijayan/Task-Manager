8d6222c41cd683093e50c3fa41d549b82337efb5
Anbarasan V <96063142+anbuvijayan@users.noreply.github.com>
2025-05-14 11:32:33 +0530

    Update axiosInstance.js

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -7,7 +7,7 @@ console.log("🚀 Axios baseURL:", BASE_URL);
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
-  timeout: 10000,
+  timeout: 30000,
   headers: {
     Accept: "application/json",
   },
